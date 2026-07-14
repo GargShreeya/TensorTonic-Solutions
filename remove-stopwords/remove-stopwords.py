d820bef60df1912bacff0e8a6a507ac893871eeb
Shreeya Garg <99465154+GargShreeya@users.noreply.github.com>
2026-07-14 16:13:42 +0530

    Solve remove-stopwords

---
+++ remove-stopwords/remove-stopwords.py
@@ -0,0 +1,9 @@
+def remove_stopwords(tokens, stopwords):
+    """
+    Returns: list[str] - tokens with stopwords removed (preserve order)
+    """
+    result = []
+    for word in tokens:
+        if word not in stopwords:
+            result.append(word)
+    return result
